d41b74d7272a7a6aaef0dd4de50d62deeb294383
Uzmaa7 <uk9971khan@gmail.com>
2023-06-09 12:58:49 +0530

    first commit

---
+++ Pw.java.java
@@ -0,0 +1,4 @@
+1. set of instructions given to computer is cALLED programme and the language through which it is given is known as programming language 
+2. main method : JVM required this method to execute the programme .
+3. java features: simple, object oriented, portable robust, platform independent, distributed.
+4. to carry out a task by computer we need a programming language.
